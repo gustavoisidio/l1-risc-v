cd716a0539c85faeda627503e22d2d0a8a5b1cff
gustavoisidio <gisf@cin.ufpe.br>
2018-08-28 17:40:30 -0300

    Reorganização da estrutura do código, levando em conta a sequência de execução de cima para baixo. e Please enter the commit message for your changes. Lines starting

--- q2.asm
+++ q2.asm
@@ -3,30 +3,20 @@ main:
     li a1, 10 # b
     li a2, 15 # c
     li a3, 16 # x
-    add a3, zero, zero
-    bge a0, zero, c1 # a >= 0
+    li a3, 0  # x = 0
+    
+    # temporary values to be compared
+    li t0, 64
+    li t1, 24
 
-c1: # a >= 0
-    li t0, 64 # t0 == 64
-    beq a1, t0, c2 # b == 64
-    blt a1, t0, c2 # b < 64
+    # comparisons
+    blt a0, zero, c1 # a >= 0
+    bgt a1, t0, c1   # b <= 64
+    beq a2, t1, c1   # c == 24
+    blt a2, t1, c1   # c < 24
+    li a3, 1         # x = 1
 
-c2: # b == 64 ou b < 64
-	li t1, 24 # t1 == 24
-    beq a2, t1, c5 # c == 24
-    # c != 24
-    li t1, 24 # t1 == 24
-    blt a2, t1, c5 # c < 24
-    # c > 24
-	li t2, 1
-    add a3, zero, t2 # x = 1
-
-c5: # c == 24 -> Não me interessa
-
-
-
-
-
+c1: 
 
 
 
