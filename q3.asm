dcbe6ac138532691f0222940c589057721cd5790
gustavoisidio <gisf@cin.ufpe.br>
2018-08-28 22:52:59 -0300

    ultima versao com bug

--- q3.asm
+++ q3.asm
@@ -10,9 +10,9 @@
 
 f1: # if (a1 < a2)
     bge a1, a2, f2
-    li x3, 15 # x13 = 15
-    add x4, x4, t0 # x14 ++
-    beq t0, t0, f3
+    li a3, 15 # x13 = 15
+    add a4, a4, t0 # x14 ++
+    beq t0, t0, f3 # pula pro final do for
 f2: # else
     li a3, 13 # x13 = 13
     add a2, a2, t0 # x12++
@@ -22,8 +22,8 @@ f3: # fim do for
     blt a0, t1, f1
 
     # a2 > a4
-    blt a2, a4, f4
-    beq a2, a4, f4
+    blt a2, a4, f4 # a2 < a4
+    beq a2, a4, f4 # a2 == a4
     li a5, 1
 
 f4:
